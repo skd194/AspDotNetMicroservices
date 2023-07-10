83c2cf36b5c8fc40f3873b3a7d0a7a53f81c3fe0
sks <shyamkumarsb007@gmail.com>
2023-07-10 10:39:26 +0530

    Repository implementation

---
+++ src/Services/Catlog/Catlog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.API.Entities;
+using System.Xml.Linq;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> Get();
+        Task<Product> Get(string id);
+        Task<IEnumerable<Product>> GetByName(string name);
+        Task<IEnumerable<Product>> GetByCategory(string categoryName);
+        Task Create(Product product);
+        Task<bool> Update(Product product);
+        Task<bool> Delete(string id);
+    }
+}
